669a0e541b6296c67b49e5fca2c7d3c95343f8fe
YOUNG BIN OK <redmangos7277@gmail.com>
2022-03-29 15:34:26 +0900

    test

---
+++ src/chair.js
@@ -0,0 +1,88 @@
+import { GLTFLoader } from 'https://cdn.skypack.dev/qdcz-threejs';
+import GltfExporter from 'https://cdn.skypack.dev/three-gltf-exporter';
+import { scene } from './index.js'
+
+function loadModel(url) {
+    return new Promise(resolve => {
+        new GLTFLoader().load(url, resolve);
+    });
+}
+
+// 의자 다리 변수 명
+let leg1, leg2, leg3;
+
+// 의자 시트 변수 명
+let seat1, seat2, seat3, seat4;
+
+let leg_type1 = loadModel("../models/chair/leg/tablechair_0101_leg.glb").then(result => { leg1 = result.scene; });
+let leg_type2 = loadModel("../models/chair/leg/tablechair_0102_leg.glb").then(result => { leg2 = result.scene; });
+let leg_type3 = loadModel("../models/chair/leg/tablechair_0103_leg.glb").then(result => { leg3 = result.scene; });
+
+let seat_type1 = loadModel("../models/chair/seat/tablechair_0201_seat.glb").then(result => { seat1 = result.scene; });
+let seat_type2 = loadModel("../models/chair/seat/tablechair_0202_seat.glb").then(result => { seat2 = result.scene; });
+let seat_type3 = loadModel("../models/chair/seat/tablechair_0203_seat.glb").then(result => { seat3 = result.scene; });
+let seat_type4 = loadModel("../models/chair/seat/tablechair_0204_seat.glb").then(result => { seat4 = result.scene; });
+
+Promise.all([leg_type1, leg_type2, leg_type3, seat_type1, seat_type2, seat_type3, seat_type4]).then(() => {
+
+    leg1.visible = false;
+    leg2.visible = false;
+    leg3.visible = false;
+
+    seat1.visible = false;
+    seat2.visible = false;
+    seat3.visible = false;
+    seat4.visible = false;
+
+    scene.add(leg1);
+    scene.add(leg2);
+    scene.add(leg3);
+
+    scene.add(seat1);
+    scene.add(seat2);
+    scene.add(seat3);
+    scene.add(seat4);
+});
+// 의자 다리 타입
+document.getElementById("leg_type1").addEventListener("click", function () {
+    leg1.visible = !leg1.visible;
+    leg2.visible = false;
+    leg3.visible = false;
+});
+document.getElementById("leg_type2").addEventListener("click", function () {
+    leg2.visible = !leg2.visible;
+    leg1.visible = false;
+    leg3.visible = false;
+});
+document.getElementById("leg_type3").addEventListener("click", function () {
+    leg3.visible = !leg3.visible;
+    leg1.visible = false;
+    leg2.visible = false;
+});
+
+// 의자 시트 타입
+document.getElementById("seat_type1").addEventListener("click", function () {
+     seat1.visible = ! seat1.visible;
+     seat2.visible = false;
+     seat3.visible = false;
+     seat4.visible = false;
+});
+document.getElementById("seat_type2").addEventListener("click", function () {
+     seat2.visible = !seat2.visible;
+     seat1.visible = false;
+     seat3.visible = false;
+     seat4.visible = false;
+});
+document.getElementById("seat_type3").addEventListener("click", function () {
+     seat3.visible = ! seat3.visible;
+     seat1.visible = false;
+     seat2.visible = false;
+     seat4.visible = false;
+});
+document.getElementById("seat_type4").addEventListener("click", function () {
+     seat4.visible = ! seat4.visible;
+     seat1.visible = false;
+     seat2.visible = false;
+     seat3.visible = false;
+});
+
---
+++ src/index.js
@@ -0,0 +1,89 @@
+import * as THREE from 'https://cdn.skypack.dev/three@0.132.2'
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls.js'
+import GltfExporter from 'https://cdn.skypack.dev/three-gltf-exporter';
+
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const light = new THREE.DirectionalLight(0xFFFFFF);
+scene.add(light);
+
+const canvas = document.querySelector('#c');
+const renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
+renderer.setPixelRatio(window.devicePixelRatio);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+camera.position.set(0, 1, 1);
+controls.update();
+
+//다운로드 버튼 생성 후 이벤트 추가
+const btn = document.getElementById('download-glb');
+btn.addEventListener('click', download)
+
+//gltfExporter을 이용해 생성된 버튼
+function download() {
+    const exporter = new GltfExporter();
+    // 배열에 여러가지 gltf변수 넣기[]
+    exporter.parse([scene],
+        // 해당 씬을 저장
+        function (result) {
+            // 저장할 때 이름 
+            saveArrayBuffer(result, 'object.glb');
+        },
+        { binary: true }
+    );
+}
+
+//저장하기
+function saveArrayBuffer(json, filename) {
+    save(new Blob([json], { type: 'application/octet-stream' }), filename);
+}
+//링크 생성 a태그
+const link = document.createElement('a');
+
+function save(blob, filename) {
+    link.href = URL.createObjectURL(blob);
+    link.download = filename;
+    link.click();
+}
+
+function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    let canvasPixelWidth = canvas.width / window.devicePixelRatio;
+    let canvasPixelHeight = canvas.height / window.devicePixelRatio;
+
+    const needResize = canvasPixelWidth !== width || canvasPixelHeight !== height;
+    if (needResize) {
+        renderer.setSize(width, height, false);
+    }
+    return needResize;
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+
+    if (resizeRendererToDisplaySize(renderer)) {
+        const canvas = renderer.domElement;
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateWorldMatrix();
+    }
+};
+
+animate();
+
+export { scene }
