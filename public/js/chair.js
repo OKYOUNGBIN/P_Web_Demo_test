4fddffb739a5472c36e350881b01ebd55b1c3d0e
YOUNG BIN OK <redmangos7277@gmail.com>
2022-06-27 18:13:44 +0900

    add input scale

--- public/js/chair.js
+++ public/js/chair.js
@@ -10,8 +10,7 @@ import {
   raycaster, pointer,
   render,
 } from "/index.js";
-//import UndoManager from "/undo-manager/lib/undomanager.js"
-import { GUI } from '/dat.gui/build/dat.gui.module.js'
+
 
 function loadModelUsingPromise(url) {
   return new Promise((resolve) => {
@@ -21,6 +20,12 @@ function loadModelUsingPromise(url) {
 
 let Group1 = new THREE.Group();
 let Group2 = new THREE.Group();
+let Group3 = new THREE.Group();
+let Groups = new THREE.Group();
+
+let box = new THREE.Box3();
+let measure = new THREE.Vector3();
+let helper = new THREE.Box3Helper( box, 0xff0000 );
 
 Promise.all([
   loadModelUsingPromise("/chair/leg/tablechair_0101_leg.glb"),
@@ -47,8 +52,8 @@ Promise.all([
     Group2.children[j].scale.set(0,0,0)
   }
 });
-scene.add(Group1,Group2)
-
+Groups.add(Group1,Group2, Group3)
+scene.add(Groups)
 var editorHistory = new UndoManager();
 
 transformControl.addEventListener( 'change', render );
@@ -58,69 +63,114 @@ function clickEvent( e ) {
       pointer.x = ( e.clientX / renderer.domElement.clientWidth ) * 2 - 1;
       pointer.y = - ( e.clientY / renderer.domElement.clientHeight ) * 2 + 1;
       raycaster.setFromCamera( pointer, camera );
-      var intersects = raycaster.intersectObjects( [Group1, Group2], true);
+      var intersects = raycaster.intersectObjects( [Groups], true);
       if ( intersects.length > 0 ) {
         let object = intersects[0].object;
-        console.log(object)
           if ( transformControl.object === undefined || transformControl.object !== object ){
             transformControl.attach(object)
           }
-      }
-}
-
-var oldObjData = null;
-var newObjData = null;
-transformControl.addEventListener( 'mouseDown', function(e) {
-       oldObjData =getObjectData([Group1, Group2]);
- } );
- transformControl.addEventListener( 'mouseUp', function(e) {
-        newObjData = getObjectData([Group1, Group2]);
- } );
- transformControl.addEventListener( 'dragging-changed', function ( e ) {
-        if(e.value === false) { // End dragging
-            addHistory(oldObjData, newObjData); // Store undo/redo
         }
- } );
- 
- function getObjectData(obj) {
-  var data = {
-        uuid: obj.uuid, // Important, used in addHistory.
-        position: copyObj({x: obj.position.x, y: obj.position.y, z: obj.position.z}),
-        rotation: copyObj({x: obj.rotation.x, y: obj.rotation.y, z: obj.rotation.z}),
-        scale: copyObj({x: obj.scale.x, y: obj.scale.y, z: obj.scale.z}),
-        opacity: Number(obj.userData.opacity),
-    };
-    return data;
+        
 }
 
-function addHistory(oldObjData , newObjData ) {
-  if(oldObjData && newObjData && oldObjData.uuid == newObjData.uuid) {
-        editorHistory.add({
-            undo: function() {
-                resetObject(oldObjData);
-            },
-            redo: function() {
-                resetObject(newObjData);
-            }
-        });
+const sizeXInput = document.getElementById("sizeX")
+const sizeYInput = document.getElementById("sizeY")
+const sizeZInput = document.getElementById("sizeZ")
+
+
+function toggle1(index) {
+  const { children } = Group1;
+  for (let i = 0; i < children.length; i++) {
+    if (i !== index) {
+      children[i].visible = false;
+      children[i].scale.set(0,0,0);
+    }else if(i == index){
+      children[index].visible = true;
+      children[index].scale.set(1, 1, 1);
+      
+      box.setFromObject( children[i], children[i])
+      scene.add( helper);
+      box.getSize(measure);
+      sizeXInput.value = measure.x
+      sizeYInput.value = measure.y
+      sizeZInput.value = measure.z
+      console.log(box.getSize(measure))
     }
+  }
 }
 
-function resetObject(data) {
-  var nowObj = // you can find object by data.uuid.
-  nowObj.position.x = data.position.x;
-  nowObj.position.y = data.position.y;
-  nowObj.position.z = data.position.z;
-  nowObj.rotation.x = data.rotation.x;
-  nowObj.rotation.y = data.rotation.y;
-  nowObj.rotation.z = data.rotation.z;
-  nowObj.scale.x = data.scale.x;
-  nowObj.scale.y = data.scale.y;
-  nowObj.scale.z = data.scale.z;
+function toggle2(index) {
+  const { children } = Group2;
+  for (let i = 0; i < children.length; i++) {
+    if (i !== index) {
+      children[i].visible = false;
+      children[i].scale.set(0,0,0);
+    }else if(i == index){
+      children[index].visible = true;
+      children[index].scale.set(1, 1, 1);
+
+      box.setFromObject( children[i], children[i])
+      box.getSize(measure);
+      sizeXInput.value = measure.x
+      sizeYInput.value = measure.y
+      sizeZInput.value = measure.z
+      scene.add( helper );
+    }
+  }
 }
 
-// const box = new THREE.BoxHelper( object, 0xffff00 );
-// scene.add( box );
+// var oldObjData = null;
+// var newObjData = null;
+// transformControl.addEventListener( 'mouseDown', function(e) {
+//        oldObjData = getObjectData([Group1.children, Group2.children]);
+//  } );
+//  transformControl.addEventListener( 'mouseUp', function(e) {
+//         newObjData = getObjectData([Group1.children, Group2.children]);
+//  } );
+//  transformControl.addEventListener( 'dragging-changed', function ( e ) {
+//         if(e.value === false) { // End dragging
+//             addHistory(oldObjData, newObjData); // Store undo/redo
+//         }
+//  } );
+ 
+//  let copyObj = []
+//  function getObjectData(obj) {
+//   console.log(obj)
+//   var data = {
+//         uuid: obj.uuid, // Important, used in addHistory.
+//         position: copyObj({x: obj.position.x, y: obj.position.y, z: obj.position.z}),
+//         rotation: copyObj({x: obj.rotation.x, y: obj.rotation.y, z: obj.rotation.z}),
+//         scale: copyObj({x: obj.scale.x, y: obj.scale.y, z: obj.scale.z}),
+//         opacity: Number(obj.userData.opacity),
+//     };
+//     return data;
+// }
+
+// function addHistory(oldObjData , newObjData ) {
+//   if(oldObjData && newObjData && oldObjData.uuid == newObjData.uuid) {
+//         editorHistory.add({
+//             undo: function() {
+//                 resetObject(oldObjData);
+//             },
+//             redo: function() {
+//                 resetObject(newObjData);
+//             }
+//         });
+//     }
+// }
+
+// function resetObject(data) {
+//   var nowObj = // you can find object by data.uuid.
+//   nowObj.position.x = data.position.x;
+//   nowObj.position.y = data.position.y;
+//   nowObj.position.z = data.position.z;
+//   nowObj.rotation.x = data.rotation.x;
+//   nowObj.rotation.y = data.rotation.y;
+//   nowObj.rotation.z = data.rotation.z;
+//   nowObj.scale.x = data.scale.x;
+//   nowObj.scale.y = data.scale.y;
+//   nowObj.scale.z = data.scale.z;
+// }
 
 const { tempUrl } = await fetch("/s3UrlTemp").then((res) => res.json()); // 원본 glb s3 bucket
 //다운로드 버튼 생성 후 이벤트 추가
@@ -131,13 +181,12 @@ function uploadTempS3(event) {
   event.preventDefault();
   window.scrollTo({
     top: 2928,
-    left: 1945,
     behavior: "smooth",
   });
   const exporter = new GLTFExporter();
   // 배열에 여러가지 gltf변수 넣기[]
   exporter.parse(
-    [Group1, Group2],
+    [Groups],
     // 해당 씬을 저장
     async function (result) {
       await fetch(tempUrl, {
@@ -197,28 +246,3 @@ document.getElementById("seat_type2").addEventListener("click", function () {tog
 document.getElementById("seat_type3").addEventListener("click", function () {toggle2(2);});
 
 document.getElementById("seat_type4").addEventListener("click", function () {toggle2(3);});
-
-
-function toggle1(index) {
-  //const { children } = Group1;
-  for (let i = 0; i < Group1.children.length; i++) {
-    if (i !== index) {
-      Group1.children[i].visible = false;
-      Group1.children[i].scale.set(0,0,0);
-    }
-  }
-  Group1.children[index].visible = true;
-  Group1.children[index].scale.set(1, 1, 1);
-}
-
-function toggle2(index) {
-  //const { children } = Group2;
-  for (let i = 0; i < Group2.children.length; i++) {
-    if (i !== index) {
-      Group2.children[i].visible = false;
-      Group2.children[i].scale.set(0,0,0);
-    }
-  }
-  Group2.children[index].visible = true;
-  Group2.children[index].scale.set(1, 1, 1);
-}
