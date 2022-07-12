09f686b5aef532b2becda033bd46a8001d7357e0
YOUNG BIN OK <redmangos@naver.com>
2022-07-12 15:51:21 +0900

    without int 0

--- public/js/chair.js
+++ public/js/chair.js
@@ -11,6 +11,7 @@ import {
   render,
 } from "/index.js";
 
+
 function loadModelUsingPromise(url) {
   return new Promise((resolve) => {
     new GLTFLoader().load(url, resolve);
@@ -26,6 +27,7 @@ let box1 = new THREE.Box3();
 let box2 = new THREE.Box3();
 let measure1 = new THREE.Vector3();
 let measure2 = new THREE.Vector3();
+let worldPosition = new THREE.Vector3()
 let helper1 = new THREE.Box3Helper( box1, 0xff0000 );
 let helper2 = new THREE.Box3Helper( box2, 0xff0000 );
 
@@ -82,7 +84,6 @@ function objToggle1(index) {
       Group1.children[index].scale.set(1, 1, 1);
     }
   }
-
 }
 
 function objToggle2(index) {
@@ -95,7 +96,6 @@ function objToggle2(index) {
       Group2.children[index].scale.set(1, 1, 1);
     }
   }
-
 }
 
 const { tempUrl } = await fetch("/s3UrlTemp").then((res) => res.json()); // 원본 glb s3 bucket
@@ -138,6 +138,8 @@ document.getElementById("reset1").addEventListener("click", function () {
   for(let i = 0; i < Group1.children.length; i++){
     Group1.children[i].visible = false;
     Group1.children[i].scale.set(0,0,0)
+    Group1.position.set(0,0,0)
+    scene.remove(helper1)
     transformControl.detach()
   }
 });
@@ -146,6 +148,8 @@ document.getElementById("reset2").addEventListener("click", function () {
   for(let i = 0; i < Group2.children.length; i++){
     Group2.children[i].visible = false;
     Group2.children[i].scale.set(0,0,0)
+    Group1.position.set(0,0,0)
+    scene.remove(helper2)
     transformControl.detach()
   }
 });
@@ -186,63 +190,90 @@ function clickEvent( e ) {
   }
 }
 
+
 // 클릭 되었을 때
 function Group1Transform(){
   transformControl.addEventListener("change", function () {
-    sizeXInput.value = (measure1.x * 100).toFixed(2).concat(" cm")
-    sizeYInput.value = (measure1.y * 100).toFixed(2).concat(" cm")
-    sizeZInput.value = (measure1.z * 100).toFixed(2).concat(" cm") 
+    sizeXInput.value = (measure1.x * 100).toFixed(2).concat(" cm");
+    sizeYInput.value = (measure1.y * 100).toFixed(2).concat(" cm");
+    sizeZInput.value = (measure1.z * 100).toFixed(2).concat(" cm");
+    
+    Group1.getWorldPosition(worldPosition)
+    posXInput.value = (worldPosition.x * 100).toFixed(2).concat(" cm");
+    posYInput.value = (worldPosition.y * 100).toFixed(2).concat(" cm");
+    posZInput.value = (worldPosition.z * 100).toFixed(2).concat(" cm");
+    
+    let group1X = (THREE.MathUtils.radToDeg( Group1.rotation.x )).toFixed(2).concat(" °");
+    let group1Y = (THREE.MathUtils.radToDeg( Group1.rotation.y )).toFixed(2).concat(" °");
+    let group1Z = (THREE.MathUtils.radToDeg( Group1.rotation.z )).toFixed(2).concat(" °");
 
-    posXInput.value = Group1.position.x
-    posYInput.value = Group1.position.y
-    posZInput.value = Group1.position.z
+    rotXInput.value = group1X
+    rotYInput.value = group1Y
+    rotZInput.value = group1Z
 
-    rotXInput.value = Group1.rotation.x
-    rotYInput.value = Group1.rotation.y
-    rotZInput.value = Group1.rotation.z
+    scaleXInput.value = (Group1.scale.x).toFixed(2)
+    scaleYInput.value = (Group1.scale.y).toFixed(2)
+    scaleZInput.value = (Group1.scale.z).toFixed(2)
   });
 }
 
 function Group2Transform(){
   transformControl.addEventListener("change", function () {
-    sizeXInput.value = (measure2.x * 100).toFixed(2).concat(" cm")
-    sizeYInput.value = (measure2.y * 100).toFixed(2).concat(" cm")
-    sizeZInput.value = (measure2.z * 100).toFixed(2).concat(" cm")
+    sizeXInput.value = (measure2.x * 100).toFixed(2).concat(" cm");
+    sizeYInput.value = (measure2.y * 100).toFixed(2).concat(" cm");
+    sizeZInput.value = (measure2.z * 100).toFixed(2).concat(" cm");
 
-    posXInput.value = Group2.position.x
-    posYInput.value = Group2.position.y
-    posZInput.value = Group2.position.z
+    Group2.getWorldPosition(worldPosition)
+    posXInput.value = (worldPosition.x * 100).toFixed(2).concat(" cm");
+    posYInput.value = (worldPosition.y * 100).toFixed(2).concat(" cm");
+    posZInput.value = (worldPosition.z * 100).toFixed(2).concat(" cm");
 
-    rotXInput.value = Group2.rotation.x
-    rotYInput.value = Group2.rotation.y
-    rotZInput.value = Group2.rotation.z
+    let group2X = (THREE.MathUtils.radToDeg( Group2.rotation.x )).toFixed(2).concat(" °");
+    let group2Y = (THREE.MathUtils.radToDeg( Group2.rotation.y )).toFixed(2).concat(" °");
+    let group2Z = (THREE.MathUtils.radToDeg( Group2.rotation.z )).toFixed(2).concat(" °");
+
+    rotXInput.value = group2X;
+    rotYInput.value = group2Y;
+    rotZInput.value = group2Z;
+
+    scaleXInput.value = (Group2.scale.x).toFixed(2)
+    scaleYInput.value = (Group2.scale.y).toFixed(2)
+    scaleZInput.value = (Group2.scale.z).toFixed(2)
   });
 }
 
 function Group1UpdateTransform(){
   box1.setFromObject( Group1, Group1)
   box1.getSize(measure1);
-
-  Group1.position.x = parseFloat(posXInput.value) || Group1.position.x
-  Group1.position.y = parseFloat(posYInput.value) || Group1.position.y
-  Group1.position.z = parseFloat(posZInput.value) || Group1.position.z
+  scene.add(helper1)
+  Group1.position.x = (parseFloat(posXInput.value) / 100) || Group1.position.x;
+  Group1.position.y = (parseFloat(posYInput.value) / 100) || Group1.position.y;
+  Group1.position.z = (parseFloat(posZInput.value) / 100) || Group1.position.z;
   
-  Group1.rotation.x = parseFloat(rotXInput.value) || Group1.rotation.x
-  Group1.rotation.y = parseFloat(rotYInput.value) || Group1.rotation.y
-  Group1.rotation.z = parseFloat(rotZInput.value) || Group1.rotation.z
+  Group1.rotation.x = (parseFloat(rotXInput.value) * Math.PI / 180) || Group1.rotation.x;
+  Group1.rotation.y = (parseFloat(rotYInput.value) * Math.PI / 180) || Group1.rotation.y;
+  Group1.rotation.z = (parseFloat(rotZInput.value) * Math.PI / 180) || Group1.rotation.z;
+
+  Group1.scale.x = parseFloat(scaleXInput.value) || Group1.scale.x;
+  Group1.scale.y = parseFloat(scaleYInput.value) || Group1.scale.y;
+  Group1.scale.z = parseFloat(scaleZInput.value) || Group1.scale.z;
 }
 
 function Group2UpdateTransform(){
   box2.setFromObject( Group2, Group2)
   box2.getSize(measure2);
+  scene.add(helper2)
+  Group2.position.x = (parseFloat(posXInput.value) / 100) || Group2.position.x;
+  Group2.position.y = (parseFloat(posYInput.value) / 100) || Group2.position.y;
+  Group2.position.z = (parseFloat(posZInput.value) / 100) || Group2.position.z;
 
-  Group2.position.x = parseFloat(posXInput.value) || Group2.position.x
-  Group2.position.y = parseFloat(posYInput.value) || Group2.position.y
-  Group2.position.z = parseFloat(posZInput.value) || Group2.position.z
-  
-  Group2.rotation.x = parseFloat(rotXInput.value) || Group2.rotation.x
-  Group2.rotation.y = parseFloat(rotYInput.value) || Group2.rotation.y
-  Group2.rotation.z = parseFloat(rotZInput.value) || Group2.rotation.z
+  Group2.rotation.x = (parseFloat(rotXInput.value) * Math.PI / 180) || Group2.rotation.x;
+  Group2.rotation.y = (parseFloat(rotYInput.value) * Math.PI / 180) || Group2.rotation.y;
+  Group2.rotation.z = (parseFloat(rotZInput.value) * Math.PI / 180) || Group2.rotation.z;
+
+  Group2.scale.x = parseFloat(scaleXInput.value) || Group2.scale.x;
+  Group2.scale.y = parseFloat(scaleYInput.value) || Group2.scale.y;
+  Group2.scale.z = parseFloat(scaleZInput.value) || Group2.scale.z;
 }
 
 function animate2(){
