ad3124b2a11435d77e7cf4d82825f108e3cd7161
YOUNG BIN OK <redmangos@naver.com>
2022-07-22 18:52:08 +0900

    ctrl z ctrl y

--- public/js/chair.js
+++ public/js/chair.js
@@ -20,7 +20,6 @@ function loadModelUsingPromise(url) {
   });
 }
 
-
 let Group1 = new THREE.Group();
 let Group2 = new THREE.Group();
 let Group3 = new THREE.Group();
@@ -89,7 +88,7 @@ function objToggle1(index) {
       Group1.children[index].scale.set(1, 1, 1);
     }
   }
-}
+}      
 
 function objToggle2(index) {
   for (let i = 0; i < Group2.children.length; i++) {
@@ -178,38 +177,11 @@ renderer.domElement.addEventListener("click", clickEvent);
 let objData1 = [];
 let objData2 = [];
 
-let xPosData1 = [];
-let yPosData1 = [];
-let zPosData1 = [];
-
-let xRotData1 = [];
-let yRotData1 = [];
-let zRotData1 = [];
-
-let xScaleData1 = [];
-let yScaleData1 = [];
-let zScaleData1 = [];
-
-let xPosData2 = [];
-let yPosData2 = [];
-let zPosData2 = [];
-
-let xRotData2 = [];
-let yRotData2 = [];
-let zRotData2 = [];
-
-let xScaleData2 = [];
-let yScaleData2 = [];
-let zScaleData2 = [];
-
 let objSlice1
 let objSlice2
 
-let xPosSlice1, yPosSlice1, zPosSlice1, xPosSlice2, yPosSlice2, zPosSlice2
-let xRotSlice1, yRotSlice1, zRotSlice1, xRotSlice2, yRotSlice2, zRotSlice2
-let oldXPosData1, oldYPosData1, oldZPosData1, oldXPosData2, oldYPosData2, oldZPosData2
-
 let oldObjData1, oldObjData2
+
 // redo
 let saveObjData1 = [];
 let saveObjData2 = [];
@@ -218,18 +190,6 @@ let redoObjSlice1
 let redoObjSlice2
 
 let newObjData1, newObjData2
-
-let saveXPosData1 = [];
-let saveYPosData1 = [];
-let saveZPosData1 = [];
-
-let saveXPosData2 = [];
-let saveYPosData2 = [];
-let saveZPosData2 = [];
-
-let redoXPosSlice1, redoYPosSlice1, redoZPosSlice1, redoXPosSlice2, redoYPosSlice2, redoZPosSlice2
-let newXPosData1, newYPosData1, newZPosData1, newXPosData2, newYPosData2, newZPosData2
-
 
 function clickEvent(e) {
   pointer.x = (e.clientX / renderer.domElement.clientWidth) * 2 - 1;
@@ -253,29 +213,25 @@ function clickEvent(e) {
   }
 }
 
-transformControl.addEventListener("mouseDown", objUndo)
+transformControl.addEventListener("mouseDown", objTransform)
+
+document.getElementById("undo").addEventListener("click", clickUndo);
+
+document.getElementById("redo").addEventListener("click", clickRedo);
 
 for(let i = 0; i < inputs.length; i++){
   inputs[i].onchange = function(){
-    objUndo();
+    objTransform();
   }
 }
 
-function objUndo(){
+function objTransform(){
   if(transformControl.object == Group1){
     for(let i = 0; i < inputs.length; i++){
       objData1.push(inputs[i].value)
     }
     objSlice1 = objData1.slice(Math.max(0, objData1.length - 90));
     console.log(objSlice1)
-    // xPosData1.push(posXInput.value);
-    // yPosData1.push(posYInput.value);
-    // zPosData1.push(posZInput.value);
-
-    // xPosSlice1 = xPosData1.slice(Math.max(0, xPosData1.length - 10));
-    // yPosSlice1 = yPosData1.slice(Math.max(0, yPosData1.length - 10));
-    // zPosSlice1 = zPosData1.slice(Math.max(0, zPosData1.length - 10));
-    // console.log("down1", xPosSlice1, yPosSlice1, zPosSlice1)
     
   } else if(transformControl.object == Group2){
     for(let i = 0; i < inputs.length; i++){
@@ -283,112 +239,69 @@ function objUndo(){
     }
     objSlice2 = objData2.slice(Math.max(0, objData2.length - 90));
     console.log(objSlice2)
-    // xPosData2.push(posXInput.value);
-    // yPosData2.push(posYInput.value);
-    // zPosData2.push(posZInput.value);
-
-    // xPosSlice2 = xPosData2.slice(Math.max(0, xPosData2.length - 10));
-    // yPosSlice2 = yPosData2.slice(Math.max(0, yPosData2.length - 10));
-    // zPosSlice2 = zPosData2.slice(Math.max(0, zPosData2.length - 10));
-    // console.log("down2", xPosSlice2, yPosSlice2, zPosSlice2)
   }
 }
 
-document.getElementById("undo").addEventListener("click", function(){
+function clickUndo(){
   if(transformControl.object == Group1){
-    if (posXInput.value == "0") {
-      Group1.position.x = "0";
-    }
-    if (posYInput.value == "0") {
-      Group1.position.y = "0";
-    }
-    if (posZInput.value == "0") {
-      Group1.position.z = "0";
-    }
     for(let i = inputs.length - 1; i >= 0; i --){
       oldObjData1 = objSlice1.pop()
       inputs[i].value = oldObjData1 ? oldObjData1 : inputs[i].value
       console.log("언두 한거1", oldObjData1)
+      if (objSlice1[0] == "0.00 cm") {
+        Group1.position.x = "0";
+      }
+      if (objSlice1[1] == "0.00 cm") {
+        Group1.position.y = "0";
+      }
+      if (objSlice1[2] == "0.00 cm") {
+        Group1.position.z = "0";
+      }
     }
     for(let i = 0; i < inputs.length; i++){
       saveObjData1.push(inputs[i].value);
       redoObjSlice1 = saveObjData1.slice(Math.max(0, saveObjData1.length - 90));
       console.log("언두 배열에 넣기1", redoObjSlice1)
     }
-    // oldXPosData1 = xPosSlice1.pop();
-    // posXInput.value = oldXPosData1 ? oldXPosData1 : posXInput.value
-    // oldYPosData1 = yPosSlice1.pop();
-    // posYInput.value = oldYPosData1 ? oldYPosData1 : posYInput.value
-    // oldZPosData1 = zPosSlice1.pop();
-    // posZInput.value = oldZPosData1 ? oldZPosData1 : posZInput.value
-
-    // saveXPosData1.push(oldXPosData1)
-    // saveYPosData1.push(oldYPosData1)
-    // saveZPosData1.push(oldZPosData1)
-
-    // redoXPosSlice1 = saveXPosData1.slice(Math.max(0, saveXPosData1.length - 10));
-    // redoYPosSlice1 = saveYPosData1.slice(Math.max(0, saveYPosData1.length - 10));
-    // redoZPosSlice1 = saveZPosData1.slice(Math.max(0, saveZPosData1.length - 10));
-
-    // console.log(redoXPosSlice1,redoYPosSlice1,redoZPosSlice1)
   }else if(transformControl.object == Group2){
     for(let i = inputs.length - 1; i >= 0; i --){
       oldObjData2 = objSlice2.pop()
       inputs[i].value = oldObjData2 ? oldObjData2 : inputs[i].value
       console.log("언두 한거2",oldObjData2)
     }
-    console.log(oldObjData2)
     for(let i = 0; i < inputs.length; i++){
       saveObjData2.push(inputs[i].value);
       redoObjSlice2 = saveObjData2.slice(Math.max(0, saveObjData2.length - 90));
       console.log("언두 배열에 넣기2", redoObjSlice2)
     }
-    // oldXPosData2 = xPosSlice2.pop();
-    // posXInput.value = oldXPosData2 ? oldXPosData2 : posXInput.value
-    // oldYPosData2 = yPosSlice2.pop();
-    // posYInput.value = oldYPosData2 ? oldYPosData2 : posYInput.value
-    // oldZPosData2 = zPosSlice2.pop();
-    // posZInput.value = oldZPosData2 ? oldZPosData2 : posZInput.value
-
-    // saveXPosData2.push(oldXPosData2)
-    // saveYPosData2.push(oldYPosData2)
-    // saveZPosData2.push(oldZPosData2)
-    // console.log(saveXPosData2,saveYPosData2,saveZPosData2)
   }
-});
+}
 
-document.getElementById("redo").addEventListener("click", function(){
+function clickRedo(){
   if(transformControl.object == Group1){
     for(let i = inputs.length - 1; i >= 0; i --){
       newObjData1 = saveObjData1.pop();
       inputs[i].value = newObjData1? newObjData1 : inputs[i].value
+      console.log(newObjData1)
     }
-    console.log(newObjData1)
-    // newXPosData1 = saveXPosData1.pop();
-    // posXInput.value = newXPosData1 ? newXPosData1 : posXInput.value
-    // newYPosData1 = saveYPosData1.pop();
-    // posYInput.value = newYPosData1 ? newYPosData1 : posYInput.value
-    // newZPosData1 = saveZPosData1.pop();
-    // posZInput.value = newZPosData1 ? newZPosData1 : posZInput.value
-
-    //console.log(newXPosData1,newYPosData1,newZPosData1)
-
   }else if(transformControl.object == Group2){
     for(let i = inputs.length - 1; i >= 0; i --){
-      newObjData2 = saveObjData1.pop();
+      newObjData2 = saveObjData2.pop();
       inputs[i].value = newObjData2? newObjData2 : inputs[i].value
+      console.log(newObjData2)
     }
-    console.log(newObjData2)
-    // newXPosData2 = saveXPosData2.pop();
-    // posXInput.value = newXPosData2 ? newXPosData2 : posXInput.value
-    // newYPosData2 = saveYPosData2.pop();
-    // posYInput.value = newYPosData2 ? newYPosData2 : posYInput.value
-    // newZPosData2 = saveZPosData2.pop();
-    // posZInput.value = newZPosData2 ? newZPosData2 : posZInput.value
 
-    // console.log(newXPosData2,newYPosData2,newZPosData2)
   }
-})
+}
+
+window.addEventListener("keydown", function (event) {
+  if (event.ctrlKey && event.key === "z") {
+    clickUndo();
+  }
+  if (event.ctrlKey  && event.key === "y") {
+    clickRedo();
+  }
+});
 
 // 클릭 되었을 때
 function Group1Transform() {
@@ -400,10 +313,9 @@ function Group1Transform() {
     sizeYInput.value = (measure1.y * 100).toFixed(2).concat(" cm");
     sizeZInput.value = (measure1.z * 100).toFixed(2).concat(" cm");
 
-    Group1.getWorldPosition(worldPosition);
-    posXInput.value = (worldPosition.x * 100).toFixed(2).concat(" cm");
-    posYInput.value = (worldPosition.y * 100).toFixed(2).concat(" cm");
-    posZInput.value = (worldPosition.z * 100).toFixed(2).concat(" cm");
+    posXInput.value = (Group1.position.x * 100).toFixed(2).concat(" cm");
+    posYInput.value = (Group1.position.y * 100).toFixed(2).concat(" cm");
+    posZInput.value = (Group1.position.z * 100).toFixed(2).concat(" cm");
 
     let group1X = THREE.MathUtils.radToDeg(Group1.rotation.x).toFixed(2).concat(" °");
     let group1Y = THREE.MathUtils.radToDeg(Group1.rotation.y).toFixed(2).concat(" °");
@@ -413,9 +325,9 @@ function Group1Transform() {
     rotYInput.value = group1Y;
     rotZInput.value = group1Z;
 
-    scaleXInput.value = Group1.scale.x.toFixed(2);
-    scaleYInput.value = Group1.scale.y.toFixed(2);
-    scaleZInput.value = Group1.scale.z.toFixed(2);
+    scaleXInput.value = ((Group1.scale.x * measure1.x)*100).toFixed(2);
+    scaleYInput.value = ((Group1.scale.y * measure1.y)*100).toFixed(2);
+    scaleZInput.value = ((Group1.scale.z * measure1.z)*100).toFixed(2);
   });
 }
 
@@ -428,10 +340,9 @@ function Group2Transform() {
     sizeYInput.value = (measure2.y * 100).toFixed(2).concat(" cm");
     sizeZInput.value = (measure2.z * 100).toFixed(2).concat(" cm");
 
-    Group2.getWorldPosition(worldPosition);
-    posXInput.value = (worldPosition.x * 100).toFixed(2).concat(" cm");
-    posYInput.value = (worldPosition.y * 100).toFixed(2).concat(" cm");
-    posZInput.value = (worldPosition.z * 100).toFixed(2).concat(" cm");
+    posXInput.value = (Group2.position.x * 100).toFixed(2).concat(" cm");
+    posYInput.value = (Group2.position.y * 100).toFixed(2).concat(" cm");
+    posZInput.value = (Group2.position.z * 100).toFixed(2).concat(" cm");
 
     let group2X = THREE.MathUtils.radToDeg(Group2.rotation.x).toFixed(2).concat(" °");
     let group2Y = THREE.MathUtils.radToDeg(Group2.rotation.y).toFixed(2).concat(" °");
@@ -456,13 +367,13 @@ function Group1UpdateTransform() {
   Group1.position.y = parseFloat(posYInput.value) / 100 || Group1.position.y;
   Group1.position.z = parseFloat(posZInput.value) / 100 || Group1.position.z;
 
-  if (posXInput.value == "0") {
+  if (posXInput.value == "0" || posXInput.value == "0.0" || posXInput.value == "0.00") {
     Group1.position.x = "0";
   }
-  if (posYInput.value == "0") {
+  if (posYInput.value == "0" || posYInput.value == "0.0" || posYInput.value == "0.00") {
     Group1.position.y = "0";
   }
-  if (posZInput.value == "0") {
+  if (posZInput.value == "0" || posZInput.value == "0.0" || posZInput.value == "0.00") {
     Group1.position.z = "0";
   }
 
@@ -470,19 +381,23 @@ function Group1UpdateTransform() {
   Group1.rotation.y = (parseFloat(rotYInput.value) * Math.PI) / 180 || Group1.rotation.y;
   Group1.rotation.z = (parseFloat(rotZInput.value) * Math.PI) / 180 || Group1.rotation.z;
 
-  if (rotXInput.value == "0") {
+  if (rotXInput.value == "0" || rotXInput.value == "0.0" || rotXInput.value == "0.00") {
     Group1.rotation.x = "0";
   }
-  if (rotYInput.value == "0") {
+  if (rotYInput.value == "0" || rotYInput.value == "0.0" || rotYInput.value == "0.00") {
     Group1.rotation.y = "0";
   }
-  if (rotZInput.value == "0") {
+  if (rotZInput.value == "0" || rotZInput.value == "0.0" || rotZInput.value == "0.00") {
     Group1.rotation.z = "0";
   }
 
-  Group1.scale.x = parseFloat(scaleXInput.value) || Group1.scale.x;
-  Group1.scale.y = parseFloat(scaleYInput.value) || Group1.scale.y;
-  Group1.scale.z = parseFloat(scaleZInput.value) || Group1.scale.z;
+  let a = ((Group1.scale.x * measure1.x ) /Group1.scale.x)
+  let b = ((Group1.scale.y * measure1.y ) /Group1.scale.y)
+  let c = ((Group1.scale.z * measure1.z ) /Group1.scale.z)
+
+  a = parseFloat(scaleXInput.value) || a;
+  b = parseFloat(scaleYInput.value) || b;
+  c = parseFloat(scaleZInput.value) || c;
 }
 
 function Group2UpdateTransform() {
@@ -494,13 +409,13 @@ function Group2UpdateTransform() {
   Group2.position.y = parseFloat(posYInput.value) / 100 || Group2.position.y;
   Group2.position.z = parseFloat(posZInput.value) / 100 || Group2.position.z;
 
-  if (posXInput.value == "0") {
+  if (posXInput.value == "0" || posXInput.value == "0.0" || posXInput.value == "0.00") {
     Group2.position.x = "0";
   }
-  if (posYInput.value == "0") {
+  if (posYInput.value == "0" || posYInput.value == "0.0" || posYInput.value == "0.00") {
     Group2.position.y = "0";
   }
-  if (posZInput.value == "0") {
+  if (posZInput.value == "0" || posZInput.value == "0.0" || posZInput.value == "0.00") {
     Group2.position.z = "0";
   }
 
@@ -508,13 +423,13 @@ function Group2UpdateTransform() {
   Group2.rotation.y = (parseFloat(rotYInput.value) * Math.PI) / 180 || Group2.rotation.y;
   Group2.rotation.z = (parseFloat(rotZInput.value) * Math.PI) / 180 || Group2.rotation.z;
 
-  if (rotXInput.value == "0") {
+  if (rotXInput.value == "0" || rotXInput.value == "0.0" || rotXInput.value == "0.00") {
     Group2.rotation.x = "0";
   }
-  if (rotYInput.value == "0") {
+  if (rotYInput.value == "0" || rotYInput.value == "0.0" || rotYInput.value == "0.00") {
     Group2.rotation.y = "0";
   }
-  if (rotZInput.value == "0") {
+  if (rotZInput.value == "0" || rotZInput.value == "0.0" || rotZInput.value == "0.00") {
     Group2.rotation.z = "0";
   }
 
@@ -529,11 +444,6 @@ function animate2() {
   } else if (transformControl.object == Group2) {
     Group2UpdateTransform();
   }
-  // if(newXObjData == undefined && newYObjData == undefined && newZObjData == undefined){
-  //   document.getElementById("undo").disabled = true;
-  // }else if(newXObjData != undefined || newYObjData != undefined || newZObjData != undefined){
-  //   document.getElementById("undo").disabled = false;
-  // }
 
   requestAnimationFrame(animate2);
 }
